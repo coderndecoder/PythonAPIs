c2da73e853f3ec198473b2ff020e07ddc760e0d3
Amruta W <amruta@Bhagyeshs-MacBook-Pro.local>
2026-03-18 17:43:27 -0500

    PUT request

--- SimpleFastAPIs/main.py
+++ SimpleFastAPIs/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 @app.get("/")
 async def read_root():
@@ -10,3 +16,7 @@ async def read_root():
 async def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
+@app.put("/item/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name}
+    
